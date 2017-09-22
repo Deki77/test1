25e032fe0767f455b1187b1e76358c7b089b4d1f
Deki <dejan.jovasevic@bitgear.rs>
2017-09-22 16:42:06 +0200

    Changed file1 to shifting algo

--- file1.c
+++ file1.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main(){
+	int sum = 0;
+	int mask = 3;
+	sum = 89 + 71;
+	int newSum = sum >> mask;
 	printf("hello world");
-	printf("My name is John");
+	printf("Sum shifted by 3 is: %d", newSum);
 }
